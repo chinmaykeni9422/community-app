61324785765a2b9158d30f0321dc8c4fb93bc8bc
chinmaykeni9422 <chinmaykeni9422@gmail.com>
2024-07-15 13:32:08 +0530

    add database connection file

---
+++ Server/src/db/database.js
@@ -0,0 +1,25 @@
+import mysql from "mysql2/promise" ;
+import ApiError from "../utils/ApiError.js" ;
+import dotenv from "dotenv" ;
+
+dotenv.config({
+    path: "./.env"
+}) ;
+
+export const pool = mysql.createPool({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    database: process.env.DATABASE,
+
+}) ;
+
+export const conectDB = async () => {
+    try {
+        const connection = await pool.getConnection() ;
+        console.log(`mySQL database Connected !!`) ;
+        connection.release() ; // Release the connection back to the pool
+    } catch (error) {
+        throw new ApiError(501, "mySQL database is not connected !!") ;
+    }
+} ;
