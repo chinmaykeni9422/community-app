78c142d904ea268b0b80b1c9558359f7ff9352ee
chinmaykeni9422 <chinmaykeni9422@gmail.com>
2024-07-25 11:43:50 +0530

    .

--- Client2/src/components/Home.jsx
+++ Client2/src/components/Home.jsx
@@ -1,11 +1,24 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import Sidebar from './Sidebar';
 import Navbar from './NavBar';
 import Profile from "./Profile"
 import AddNumber from './AddNumber';
 import Inbox from './Inbox';
+import { useNavigate } from 'react-router-dom';
+
 
 const HomePage = () => {
+
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        const userInfo = JSON.parse(localStorage.getItem('userInfo'));
+
+        if (!userInfo || !userInfo.token) {
+            // If no token or userInfo is found, redirect to login or profile page
+            navigate('/login'); // or any other page you want to redirect to
+        }
+    }, [navigate]);
 
     const [sideBarTogggle, setSideBarToggle] = useState(false);
     const [activeComponent, setActiveComponent] = useState('profile');
