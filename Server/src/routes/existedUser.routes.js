2d8bda068077f3feaed0fe27f6bcdc9c733ebf43
chinmaykeni9422 <chinmaykeni9422@gmail.com>
2024-08-30 14:59:37 +0530

    .

--- Server/src/routes/existedUser.routes.js
+++ Server/src/routes/existedUser.routes.js
@@ -7,5 +7,6 @@ router.post("/login", checkUserLogin) ;
 router.post("/addNumber", addUserNumber) ;
 router.get("/Numbers/:user_id", getNumbers) ;
 router.post("/profileUpdate", UpdateProfile) ;
+router.get('/config', getConfig);
 
 export default router;
